2a9dfe4ae42816034b3f129b3d58da7989c792f8
Matthew Franchini <matchewf11@gmail.com>
2026-01-12 22:24:06 -0800

    feat: progrss

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,17 @@
 use axum::{Router, routing::get};
 use tokio::net::TcpListener;
 
+struct ChatsGet {
+    body: String,
+}
+
+async fn chats_get() {
+    todo!();
+}
+
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(|| async { "hello world" }));
+    let router = Router::new().route("/chats", get(chats_get));
     let listener = TcpListener::bind("0.0.0.0:3000").await.unwrap();
-
-    println!("Running on port 3000");
-    axum::serve(listener, app).await.unwrap();
+    axum::serve(listener, router).await.unwrap();
 }
