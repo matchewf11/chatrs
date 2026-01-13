0ce4de342c303935bc4a68049c0111ca5af3d044
Matthew Franchini <matchewf11@gmail.com>
2026-01-12 19:33:32 -0800

    updated main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{Router, routing::get};
+use tokio::net::TcpListener;
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(|| async { "hello world" }));
+    let listener = TcpListener::bind("0.0.0.0:3000").await.unwrap();
+
+    println!("Running on port 3000");
+    axum::serve(listener, app).await.unwrap();
 }
