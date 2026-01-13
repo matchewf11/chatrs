770554919ff1be2f11effad4effa38a6a6eb632c
Matthew Franchini <matchewf11@gmail.com>
2026-01-13 01:12:06 -0800

    fix: cleaned up paths in main

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
+use chatrs::{app, db};
 use std::error::Error;
 use tokio::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    let _pool = chatrs::db::new().await?;
-    let router = chatrs::app::new();
+    let _pool = db::new().await?;
+    let router = app::new();
     let listener = TcpListener::bind("0.0.0.0:3000").await?;
     axum::serve(listener, router).await?;
     Ok(())
